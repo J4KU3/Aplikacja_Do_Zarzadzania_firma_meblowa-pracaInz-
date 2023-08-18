d7bd4e76a44b9b20e78232c0fec5beca5616c56b
Jakub <kuba24481@wp.pl>
2023-08-18 16:56:19 +0200

    Create Base Command and ViewModel and MainViewModel

---
+++ SystemDoZarzadzaniaFirmaMeblowa/SystemDoZarzadzaniaFirmaMeblowa/Commands/BaseCommand/CommandBase.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace SystemDoZarzadzaniaFirmaMeblowa.Commands.BaseCommand
+{
+    public abstract class CommandBase : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        public virtual bool CanExecute(object parameter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual void Execute(object parameter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, new EventArgs());
+        }
+    }
+}
---
+++ SystemDoZarzadzaniaFirmaMeblowa/SystemDoZarzadzaniaFirmaMeblowa/ViewModels/MainViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SystemDoZarzadzaniaFirmaMeblowa.ViewModels
+{
+    public class MainViewModel:BaseViewModel
+    {
+        public MainViewModel()
+        {
+
+        }
+    }
+}
