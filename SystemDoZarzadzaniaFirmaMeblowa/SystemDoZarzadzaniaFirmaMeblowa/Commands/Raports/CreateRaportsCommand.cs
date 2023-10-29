62d6ef7b66d2360bd5d8ebb495ca86f5bec156cb
Jakub <kuba24481@wp.pl>
2023-10-29 19:00:22 +0100

    Start Create New Function

---
+++ SystemDoZarzadzaniaFirmaMeblowa/SystemDoZarzadzaniaFirmaMeblowa/Commands/Raports/CreateRaportsCommand.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SystemDoZarzadzaniaFirmaMeblowa.Commands.BaseCommand;
+using SystemDoZarzadzaniaFirmaMeblowa.ViewModels;
+
+namespace SystemDoZarzadzaniaFirmaMeblowa.Commands.Raports
+{
+    public class CreateRaportsCommand:CommandBase
+    {
+        private readonly MainViewModel _mainviewModel;
+
+        public CreateRaportsCommand(MainViewModel mainViewModel)
+        {
+            _mainviewModel = mainViewModel;
+        }
+
+        public override bool CanExecute(object parameter)
+        {
+            return base.CanExecute(parameter);
+        }
+
+        public override void Execute(object parameter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
