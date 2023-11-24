3aa60ffe7237fddcff03aeb3772ff67e8cc965c5
Jakub <kuba24481@wp.pl>
2023-11-24 16:54:12 +0100

    EditComplaints and view Done

--- SystemDoZarzadzaniaFirmaMeblowa/SystemDoZarzadzaniaFirmaMeblowa/Commands/ComplaintsFolder/EditComplaintsCommand.cs
+++ SystemDoZarzadzaniaFirmaMeblowa/SystemDoZarzadzaniaFirmaMeblowa/Commands/ComplaintsFolder/EditComplaintsCommand.cs
@@ -3,10 +3,52 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using SystemDoZarzadzaniaFirmaMeblowa.Commands.BaseCommand;
+using SystemDoZarzadzaniaFirmaMeblowa.Data;
+using SystemDoZarzadzaniaFirmaMeblowa.Models;
+using SystemDoZarzadzaniaFirmaMeblowa.ViewModels;
+
 
 namespace SystemDoZarzadzaniaFirmaMeblowa.Commands.ComplaintsFolder
 {
-    class EditComplaintsCommand
+   public class EditComplaintsCommand:CommandBase
     {
+        private readonly MainViewModel _mainviewModel;
+
+        public EditComplaintsCommand(MainViewModel mainViewModel)
+        {
+            _mainviewModel = mainViewModel;
+        }
+        public override bool CanExecute(object parameter)
+        {
+            return _mainviewModel.SelectedComplaints != null;
+        }
+        public override void Execute(object parameter)
+        {
+            try
+            {
+                using (var resource = new ZarzadzanieFirmaDBEntities())
+                {
+                    var ComplaintToEdit = resource.Complaints.FirstOrDefault(x => x.IdComplaint == _mainviewModel.SelectedComplaints.IdComplaint);
+                    var selectedComplaint = _mainviewModel.SelectedComplaints;
+                    if (ComplaintToEdit != null)
+                    {
+                        
+                        ComplaintToEdit.Reason = selectedComplaint.Reason;
+  
+                        resource.SaveChanges();
+                        MessageBox.Show($"Pracownik o id:{ComplaintToEdit.IdComplaint} został edytowany ");
+                    }
+
+
+                }
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("Żadna reklamacja nie została zaznaczona");
+            }
+        }
     }
 }
